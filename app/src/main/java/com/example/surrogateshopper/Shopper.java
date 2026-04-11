c7f6aaa77e95e4d1755700fcee489474b31f773d
Mpho-source <mphomk2023@gmail.com>
2026-04-11 23:50:49 +0200

    Added more functionality

--- app/src/main/java/com/example/surrogateshopper/Shopper.java
+++ app/src/main/java/com/example/surrogateshopper/Shopper.java
@@ -2,13 +2,22 @@ package com.example.surrogateshopper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.R.layout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class Shopper extends AppCompatActivity {
 
@@ -23,11 +32,17 @@ public class Shopper extends AppCompatActivity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("Shopper_Name");
 
-        hi.setText("Hi, " + name);
+        hi.setText("Hi \uD83D\uDC4B " + name);
 
 
 
-        Button btnAdd = findViewById(R.id.btnAdd);
+        FloatingActionButton btnAdd = findViewById(R.id.btnAdd);
+        Button btnGo = findViewById(R.id.btnGo);
+        Button btnDone = findViewById(R.id.btnDone);
+        EditText etProduct = findViewById(R.id.etProduct);
+        ArrayList<String> products = new ArrayList<>();
+        ListView productsList = findViewById(R.id.productsList);
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,10 +60,66 @@ public class Shopper extends AppCompatActivity {
                 Button btnDone = findViewById(R.id.btnDone);
                 btnDone.setVisibility(View.VISIBLE);
 
+                TextView welcomeShopper = findViewById(R.id.welcomeShopper);
+                welcomeShopper.setVisibility(View.INVISIBLE);
+
+                TextView textList = findViewById(R.id.textList);
+                textList.setText("");
                 String prod = etProduct.getText().toString();
                 System.out.println(prod);
             }
         });
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String prodEntered = etProduct.getText().toString();
+                products.add(prodEntered);
+
+                String prod = etProduct.getText().toString();
+                //Toast.makeText(Shopper.this, "Added " + prod, Toast.LENGTH_SHORT).show();
+                Snackbar.make(btnGo, "Added " + prod, Snackbar.LENGTH_SHORT).setAction("Close", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                })
+                .show();
+
+                etProduct.setText("");
+
+
+
+
+
+
+            }
+        });
+
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                productsList.setVisibility(View.VISIBLE);
+                etProduct.setVisibility(View.INVISIBLE);
+                btnDone.setVisibility(View.INVISIBLE);
+                btnGo.setVisibility(View.INVISIBLE);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(Shopper.this, layout.simple_list_item_1, products);
+                productsList.setAdapter(adapter);
+
+                TextView textList = findViewById(R.id.textList);
+                textList.setText("MY LIST");
+
+
+
+            }
+        });
+
+
     }
 
 
